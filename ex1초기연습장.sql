f4a6b83184aa8966bf8b5ccffc06548b6cb24651
doochan-ctrl <dduxhani@gmail.com>
2025-12-31 12:11:54 +0900

    db-practice-sql

---
+++ ex1초기연습장.sql
@@ -0,0 +1,7 @@
+-- 샘플 테이블 조회 해보기
+-- vscode에서 실행하는 방법, 한문장 단위 실행: command + Enter, Ctrl + Enter
+-- 주의사항 최초 실행시 vscode 측에서 서버를 선택하는 메뉴가 나오면
+--이전에 설치한 연결 설정을 확인하기
+select * from emp;
+-- 현재 시간조회
+select sysdate FROM dual;
